7268c5c7fc56337a8dbc55b40d7f6005ebcc1323
Mahi samia <mahisamia13@gmail.com>
2021-10-05 16:49:07 +0200

    setup models migrations and relations

---
+++ database/migrations/2021_10_05_144147_create_posts_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts_tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')
+                  ->constrained()
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->foreignId('tag_id')
+                  ->constrained()
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts_tags');
+    }
+}
